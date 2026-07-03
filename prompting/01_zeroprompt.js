dc46be056d36bb5c5ed9bfe40152210acc21110b
Sagar <waddenkerisagar@gmail.com>
2026-07-03 22:52:20 +0530

    Add initial implementation for OpenAI chat completion

--- prompting/01_zeroprompt.js
+++ prompting/01_zeroprompt.js
@@ -3,3 +3,17 @@ import { OpenAI } from "openai";
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+async function main() {
+  client.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "user",
+        content: "You are an expert tell me what is 2+2",
+      },
+    ],
+  });
+}
+
+main();
